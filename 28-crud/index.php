450694a4ba092a4142a7a08ea274f81564dd70ba
Henrique <henrique-lopes@outlook.com.br>
2022-04-29 10:50:38 -0300

    inserção dos arquivos da pasta crud

--- 28-crud/index.php
+++ 28-crud/index.php
@@ -27,7 +27,7 @@ include_once('includes/header.php');
       </tbody>
     </table>
     <br>
-    <a href="" class="btn">Adicionar cliente </a>
+    <a href="adicionar.php" class="btn">Adicionar cliente </a>
   </div>
 </div>
 
---
+++ 28-crud/php_action/create.php
@@ -0,0 +1,22 @@
+<?php
+//conexão
+require_once 'db_connect.php';
+
+if(isset($_POST['btn-cadastrar'])):
+
+print_r($_POST);
+$nome = mysqli_escape_string($connect, $_POST['nome']);
+$sobrenome = mysqli_escape_string($connect, $_POST['sobrenome']);
+$email = mysqli_escape_string($connect, $_POST['email']);
+$idade = mysqli_escape_string($connect, $_POST['idade']);
+
+
+$sql = "INSERT INTO clientes (nome,sobrenome,email,idade) VALUES ('$nome', '$sobrenome', '$email', '$idade' )";
+
+    if(mysqli_query($connect,$sql)):
+        header('Location: ../index.php?sucesso');
+       
+    else:
+        header('Location: ../index.php?erro');
+    endif;   
+endif;
